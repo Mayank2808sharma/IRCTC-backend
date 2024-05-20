992dc686f2c82f960761abc591af75e02eba8b1b
Puskar-Roy <puskarroy600@gmail.com>
2024-05-20 23:36:49 +0530

    feat : change password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, changeUserPassword } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/change-password', changeUserPassword);
 
 module.exports = router;
